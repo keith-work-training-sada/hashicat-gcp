82c073a4f20caff46183a17707eec15adefb7c45
keithhopkins <keith.hopkins+tf.training.2023@sada.com>
2023-07-06 01:33:13 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,7 @@
-module "vpc" {
-  source  = "terraform-google-modules/network/google"
-  version = "~> 3.0"
+module "network" {
+  source  = "app.terraform.io/sada-partner-training-20230705/network/google"
+  version = "3.4.0"
+  # insert required variables here
 
   project_id   = var.project
   network_name = "gaurav-network"
