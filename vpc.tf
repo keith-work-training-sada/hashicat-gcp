7c7f46dbb627a7caba2c3c4e9173c4416b978318
keithhopkins <keith.hopkins+tf.training.2023@sada.com>
2023-07-06 01:35:25 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -4,13 +4,13 @@ module "network" {
   # insert required variables here
 
   project_id   = var.project
-  network_name = "gaurav-network"
+  network_name = "keith-network"
   routing_mode = "GLOBAL"
 
   subnets = [
     {
-      subnet_name   = "gaurav-subnet"
-      subnet_ip     = "10.100.10.0/24"
+      subnet_name   = "keith-subnet"
+      subnet_ip     = "10.101.10.0/24"
       subnet_region = var.region
     }
   ]
